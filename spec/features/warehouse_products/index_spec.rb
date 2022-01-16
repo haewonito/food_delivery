b5e3abb7d5c3f40ca3986452d4ca3c9eac13cc21
Haewon Jeon <haewonito@gmail.com>
2022-01-16 09:29:18 -0700

    committing before fixing inventory_index

--- spec/features/warehouse_products/index_spec.rb
+++ spec/features/warehouse_products/index_spec.rb
@@ -4,35 +4,31 @@ RSpec.describe "Inventories Index Page", type: :feature do
   describe "as a visitor" do
     before(:each) do
 
-      @potatoes = Product.create!(name: "Potatoes", unit_price: 15.65, storage_requirement: "rt_perishable")
-      @onions = Product.create!(name: "Onions", unit_price: 12.50, storage_requirement: "rt_perishable")
-      @tomatoes = Product.create!(name: "Tomatoes", unit_price: 7.50, storage_requirement: "refrigerated")
-      @milk = Product.create!(name: "Milk", unit_price: 3.99, storage_requirement: "refrigerated")
-      @eggs = Product.create!(name: "Eggs", unit_price: 4.99, storage_requirement: "refrigerated")
-      @puff_pastry = Product.create!(name: "Puff Pastry", unit_price: 19.99, storage_requirement: "frozen")
-      @shrimp = Product.create!(name: "Shrimp", unit_price: 15.99, storage_requirement: "frozen")
-      @rice = Product.create!(name: "Rice", unit_price: 10.00, storage_requirement: "non_perishable")
-      @beans = Product.create!(name: "Beans", unit_price: 10.00, storage_requirement: "non_perishable")
-      @linguine = Product.create!(name: "Linguine", unit_price: 18.05, storage_requirement: "non_perishable")
+    @milk = Product.create!(name: "Milk", quantity_available: 100, unit_price: 3.99, storage_requirement: "refrigerated")
+    @eggs = Product.create!(name: "Eggs", quantity_available: 100, unit_price: 4.99, storage_requirement: "refrigerated")
+    @puff_pastry = Product.create!(name: "Puff Pastry", quantity_available: 100, unit_price: 19.99, storage_requirement: "frozen")
+    @shrimp = Product.create!(name: "Shrimp", quantity_available: 100, unit_price: 15.99, storage_requirement: "frozen")
+    @linguine = Product.create!(name: "Linguine", quantity_available: 100, unit_price: 18.05, storage_requirement: "non_perishable")
 
-      @denver_warehouse = Warehouse.create!(location_name: "Denver", phone_number: "395-394-3958", address: "4051 Honker St., Denver, CO 80202")
-      @aurora_warehouse = Warehouse.create!(location_name: "Aurora", phone_number: "720-486-2958", address: "357 Lansing Ct, Aurora, CO 39573")
-      @fort_collins_warehouse = Warehouse.create!(location_name: "Fort Collins", phone_number: "303-249-2059", address: "305 S College Ave., Fort Collins, CO 80525")
+    @denver_warehouse = Warehouse.create!(location_name: "Denver", phone_number: "395-394-3958", address: "4051 Honker St., Denver, CO 80202")
+    @aurora_warehouse = Warehouse.create!(location_name: "Aurora", phone_number: "720-486-2958", address: "357 Lansing Ct, Aurora, CO 39573")
+    @fort_collins_warehouse = Warehouse.create!(location_name: "Fort Collins", phone_number: "303-249-2059", address: "305 S College Ave., Fort Collins, CO 80525")
 
-      @inv1 = WarehouseProduct.create!(unit_price: 15.65, quantity: 2, total_value: 31.3, product_id: @potatoes.id, warehouse_id: @denver_warehouse.id)
-      @inv2 = WarehouseProduct.create!(unit_price: 15.65, quantity: 10, total_value: 156.50, product_id: @potatoes.id, warehouse_id: @aurora_warehouse.id)
-      @inv3 = WarehouseProduct.create!(unit_price: 12.50, quantity: 10, total_value: 120.50, product_id: @onions.id, warehouse_id: @denver_warehouse.id)
-      @inv4 = WarehouseProduct.create!(unit_price: 12.50, quantity: 10, total_value: 120.50, product_id: @onions.id, warehouse_id: @fort_collins_warehouse.id)
-      @inv5 = WarehouseProduct.create!(unit_price: 7.50, quantity: 8, total_value: 60.00, product_id: @tomatoes.id, warehouse_id: @fort_collins_warehouse.id)
-      @inv6 = WarehouseProduct.create!(unit_price: 7.50, quantity: 10, total_value: 75.00, product_id: @tomatoes.id, warehouse_id: @denver_warehouse.id)
-      @inv7 = WarehouseProduct.create!(unit_price: 7.50, quantity: 2, total_value: 15.00, product_id: @tomatoes.id, warehouse_id: @aurora_warehouse.id)
-      @inv8  = WarehouseProduct.create!(unit_price: 3.99, quantity: 40, total_value: 159.60, product_id: @milk.id, warehouse_id: @aurora_warehouse.id)
-      @inv9  = WarehouseProduct.create!(unit_price: 3.99, quantity: 5, total_value: 19.95, product_id: @milk.id, warehouse_id: @fort_collins_warehouse.id)
+    @inv1 = WarehouseProduct.create!(quantity: 2, product_id: @eggs.id, warehouse_id: @denver_warehouse.id)
+    @inv2 = WarehouseProduct.create!(quantity: 10, product_id: @eggs.id, warehouse_id: @aurora_warehouse.id)
+    @inv3 = WarehouseProduct.create!(quantity: 10, product_id: @linguine.id, warehouse_id: @denver_warehouse.id)
+    @inv4 = WarehouseProduct.create!(quantity: 10, product_id: @linguine.id, warehouse_id: @fort_collins_warehouse.id)
+    @inv5 = WarehouseProduct.create!(quantity: 8, product_id: @puff_pastry.id, warehouse_id: @fort_collins_warehouse.id)
+    @inv6 = WarehouseProduct.create!(quantity: 10, product_id: @puff_pastry.id, warehouse_id: @denver_warehouse.id)
+    @inv7 = WarehouseProduct.create!(quantity: 2, product_id: @puff_pastry.id, warehouse_id: @aurora_warehouse.id)
+    @inv8  = WarehouseProduct.create!(quantity: 40, product_id: @milk.id, warehouse_id: @aurora_warehouse.id)
+    @inv9  = WarehouseProduct.create!(quantity: 5, product_id: @milk.id, warehouse_id: @fort_collins_warehouse.id)
+    @inv10 = WarehouseProduct.create!(quantity: 10, product_id: @shrimp.id, warehouse_id: @denver_warehouse.id)
 
-      visit warehouse_products_path
+    visit warehouse_products_path
     end
 
-    it "I see a list of warehouse_product items with details" do
+    it "I see a list of inventory items with details" do
       expect(page).to have_css(".warehouse_product", count: 9)
 
       within(first('.warehouse_product')) do
@@ -55,14 +51,14 @@ RSpec.describe "Inventories Index Page", type: :feature do
       end
     end
 
-    it "for each warehouse_product, I see a button to edit" do
+    xit "for each inventory, I see a button to edit" do
       within(first('.warehouse_product')) do
         click_link "Edit"
         expect(current_path).to eq(edit_warehouse_product_path(@inv1.id))
       end
     end
 
-    it "for each warehouse_product, I see a button to delete and it deletes" do
+    xit "for each inventory, I see a button to delete and it deletes" do
       within(first('.warehouse_product')) do
         click_link "Delete"
         expect(current_path).to eq(warehouse_products_path)
