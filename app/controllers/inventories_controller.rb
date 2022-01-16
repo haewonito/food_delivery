8448dd73981ea213e6ddcffc37b9795183381a3a
Haewon Jeon <haewonito@gmail.com>
2022-01-16 11:49:59 -0700

    refactor: moved warehouse_products page to inventories. need to move tests next

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,48 +1,40 @@
-class WarehouseProductsController < ApplicationController
+class InventoriesController < ApplicationController
   def index
     @warehouse_products = WarehouseProduct.all
-    @products
-  end
-
-  def show
-    @warehouse_product = WarehouseProduct.find(params[:id])
   end
 
   def new
+    # @warehouse = Warehouse.find(params)
   end
 
   def create
-
     @warehouse_product = WarehouseProduct.create(warehouse_product_params)
-    @products = Product.all
-    @warehouse_products = WarehouseProduct.all
-
-    # redirect_to :back
-    redirect_to warehouse_products_path
+    # if @warehouse_product.save
+    redirect_to "/warehouse_products"
+    #   flash[:notice] = "New Inventory Is Successfully Created!"
+    # else
+    #   redirect_to "/warehouse_products/new"
+    #   flash[:alert] = "Error: Try Again"
+    # end
   end
 
   def edit
     @warehouse_product = WarehouseProduct.find(params[:id])
-
-    # if params[:add]
-    #   @warehouse_product.quantity +
-    #
   end
 
   def update
     warehouse_product = WarehouseProduct.find(params[:id])
     warehouse_product.update(warehouse_product_params)
 
-    redirect_to warehouse_products_path
+    redirect_to "/warehouse_products"
   end
 
   def destroy
     WarehouseProduct.find(params[:id]).destroy
-    redirect_to warehouse_products_path
+    redirect_to "/warehouse_products"
   end
 
   private
-
   def warehouse_product_params
     params.permit(:product_id, :quantity, :warehouse_id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,17 @@ Rails.application.routes.draw do
   #   resources :warehouse_products
   # end
   post "/warehouses/:warehouse_id/products/new", to: "warehouse_products#create"
+
   resources :warehouses do
     resources :products, controller: :warehouse_products
   end
   resources :products
-  # resources :warehouse_products
+  # resources :warehouse_products, controller: :inventories
+  get "/warehouse_products", to: "inventories#index"
+  get "/warehouse_products/new", to: "inventories#new"
+  post "/warehouse_products", to: "inventories#create"
+  get "/warehouse_products/:id/edit", to: "inventories#edit"
+  patch "/warehouse_products/:id", to: "inventories#update"
+  delete "/warehouse_products/:id", to: "inventories#destroy"
 
 end
