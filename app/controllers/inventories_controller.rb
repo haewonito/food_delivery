1ab25fb0bf8b0550c655ecf08ffbd27e4f7c48eb
Haewon Jeon <haewonito@gmail.com>
2022-01-17 11:55:53 -0700

    refactor: controllers cleaned up

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -4,18 +4,11 @@ class InventoriesController < ApplicationController
   end
 
   def new
-    # @warehouse = Warehouse.find(params)
   end
 
   def create
     @warehouse_product = WarehouseProduct.create(warehouse_product_params)
-    # if @warehouse_product.save
     redirect_to "/warehouse_products"
-    #   flash[:notice] = "New Inventory Is Successfully Created!"
-    # else
-    #   redirect_to "/warehouse_products/new"
-    #   flash[:alert] = "Error: Try Again"
-    # end
   end
 
   def edit
@@ -25,7 +18,6 @@ class InventoriesController < ApplicationController
   def update
     warehouse_product = WarehouseProduct.find(params[:id])
     warehouse_product.update(warehouse_product_params)
-
     redirect_to "/warehouse_products"
   end
 
